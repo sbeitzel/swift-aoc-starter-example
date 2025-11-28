74350f9fca5fb05bcd54a26d9635a1e78412e941
Stephen Beitzel <sbeitzel@pobox.com>
2025-11-28 09:51:29 -0800

    include day 1 solution

--- Sources/Day01.swift
+++ Sources/Day01.swift
@@ -1,18 +1,52 @@
 import Foundation
 import Parsing
 
-/// Not actually 2025 day 1, this is me noodling with 2024 day 4
 final class Day01: AdventDay {
 
+  let parsedListA: [Int]
+  let parsedListB: [Int]
+
   init(data: String) {
+    let locationIDTupleParser = Parse(input: Substring.self) {
+        Int.parser()
+        "   "
+        Int.parser()
+    }
+    // split the data string into lines
+    let inputLines = data.split(separator: "\n").map { String($0) }
+    var listA: [Int] = []
+    var listB: [Int] = []
+    for line in inputLines {
+      var modifiableInput = line[...]
+      do {
+        let tuple = try locationIDTupleParser.parse(&modifiableInput)
+        listA.append(tuple.0)
+        listB.append(tuple.1)
+      } catch {
+        print("Error parsing input: \(error.localizedDescription)")
+      }
+    }
+
+    self.parsedListA = listA
+    self.parsedListB = listB
   }
 
   func part1() -> Any {
-    return ""
+    var list: [Int] = []
+    for pair in zip(parsedListA.sorted(), parsedListB.sorted()) {
+        list.append(abs(pair.0 - pair.1))
+    }
+
+    return "\(list.reduce(0) { $0 + $1 })"
   }
 
   func part2() -> Any {
-    return ""
+    var frequencies: [Int: Int] = [:]
+    for number in parsedListB {
+        frequencies[number, default: 0] += 1
+    }
+
+    return "\(parsedListA.reduce(0) { $0 + frequencies[$1, default: 0] * $1 })"
   }
 
 }
--- Tests/Day01.swift
+++ Tests/Day01.swift
@@ -5,17 +5,22 @@ import Testing
 final class Day01Tests {
 
   let testData = """
-
+    3   4
+    4   3
+    2   5
+    1   3
+    3   9
+    3   3
     """
 
   @Test func testPart1() async throws {
     let challenge = Day01(data: testData)
-    #expect(String(describing: challenge.part1()) == "")
+    #expect(String(describing: challenge.part1()) == "11")
   }
 
   @Test func testPart2() async throws {
     let challenge = Day01(data: testData)
-    #expect(String(describing: challenge.part2()) == "")
+    #expect(String(describing: challenge.part2()) == "31")
   }
 
 }
