3d129e53a14489bafbd1fdadd355aa71b2cc9f84
Stephen Beitzel <sbeitzel@pobox.com>
2025-11-28 18:02:51 -0800

    port over day 3

--- Sources/Day03.swift
+++ Sources/Day03.swift
@@ -1,16 +1,90 @@
 import Foundation
+import Parsing
 
 final class Day03: AdventDay {
 
+  let instructions: [Day03Solver.Instruction]
+
   init(data: String) {
+    let mulParser = Parse(input: Substring.self) {
+        "mul("
+        Int.parser()
+        ","
+        Int.parser()
+        ")"
+    }.map { Day03Solver.Instruction.multiply($0, $1)}
+
+    let doParser = Parse(input: Substring.self) {
+        "do()"
+    }.map { Day03Solver.Instruction.process }
+
+    let doNotParser = Parse(input: Substring.self) {
+        "don't()"
+    }.map { Day03Solver.Instruction.noProcessing }
+
+    let memoryParser = OneOf {
+        mulParser
+        doParser
+        doNotParser
+    }
+    var modifiableInput = data[...]
+    var instructions: [Day03Solver.Instruction] = []
+    while !modifiableInput.isEmpty {
+        do {
+            let instruction = try memoryParser.parse(&modifiableInput)
+            // it worked!
+            instructions.append(instruction)
+        } catch {
+            // okay, so, pop off the first character and try again
+            _ = modifiableInput.popFirst()
+        }
+    }
+    self.instructions = instructions
   }
 
   func part1() -> Any {
-    return ""
+    let solver = Day03Solver(stack: instructions.filter({ $0 != .noProcessing && $0 != .process }))
+    solver.runStack()
+    return "\(solver.result)"
   }
 
   func part2() -> Any {
-    return ""
+    let solver = Day03Solver(stack: instructions)
+    solver.runStack()
+    return "\(solver.result)"
   }
 
+}
+
+class Day03Solver {
+    enum Instruction: Equatable {
+        case multiply(Int, Int)
+        case noProcessing
+        case process
+    }
+
+    var stack: [Instruction]
+    var result: Int
+
+    init(stack: [Instruction]) {
+        self.result = 0
+        self.stack = stack
+    }
+
+    func runStack() {
+        var isProcessing = true
+        while !stack.isEmpty {
+            let instruction = stack.removeFirst()
+            switch instruction {
+            case .multiply(let multiplier, let multiplier2):
+                if isProcessing {
+                    result += multiplier * multiplier2
+                }
+            case .noProcessing:
+                isProcessing = false
+            case .process:
+                isProcessing = true
+            }
+        }
+    }
 }
--- Tests/Day03.swift
+++ Tests/Day03.swift
@@ -4,18 +4,21 @@ import Testing
 
 final class Day03Tests {
 
-  let testData = """
-
+  let testDataP1 = """
+    xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+    """
+  let testDataP2 = """
+    xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
     """
 
   @Test func testPart1() async throws {
-    let challenge = Day03(data: testData)
-    #expect(String(describing: challenge.part1()) == "")
+    let challenge = Day03(data: testDataP1)
+    #expect(String(describing: challenge.part1()) == "161")
   }
 
   @Test func testPart2() async throws {
-    let challenge = Day03(data: testData)
-    #expect(String(describing: challenge.part2()) == "")
+    let challenge = Day03(data: testDataP2)
+    #expect(String(describing: challenge.part2()) == "48")
   }
 
 }
